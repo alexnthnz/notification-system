cdcdd9b27c917e2ea664588761cb994bcf5285f8
Alex Nguyen <alexnguyenth96@gmail.com>
2025-08-03 22:12:39 +1200

    Update

--- api/grpc/server.go
+++ api/grpc/server.go
@@ -4,14 +4,14 @@ import (
 	"context"
 	"time"
 
+	"go.uber.org/zap"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
-	"go.uber.org/zap"
 
 	pb "github.com/alexnthnz/notification-system/api/proto/gen"
-	"github.com/alexnthnz/notification-system/internal/notification"
 	"github.com/alexnthnz/notification-system/internal/monitoring"
+	"github.com/alexnthnz/notification-system/internal/notification"
 )
 
 // Server implements the NotificationService gRPC server
